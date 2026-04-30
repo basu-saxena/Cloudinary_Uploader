c345cc6b794f680f22ea0723264b9d1843394074
basu-saxena <basusaxena2003@gmail.com>
2026-04-30 21:03:26 +0530

    setup express server

---
+++ server.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+const { app } = require("./src/main");
+const PORT = process.env.PORT;
+// const { connectToDB } = require("./src/lib/database");
+
+app.listen(PORT, () => console.log("Server is running"));
